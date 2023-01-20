9006da3dcfc5f3f6ef2a2df19d37c8925120cf34
HughesSeanKyle <khughessean@yahoo.com>
2023-01-20 15:56:00 +0200

    Store logic and retrieve

--- contracts/DeStore.sol
+++ contracts/DeStore.sol
@@ -3,24 +3,33 @@ pragma solidity ^0.8.0;
 
 contract DecentralizedStorage {
     mapping(address => bool) private owners;
+    mapping(address => bytes) private data;
 
     function addOwner(address _owner) public {
-        require(msg.sender == msg.sender, "Only the contract deployer can add new owners.");
+        require(
+            msg.sender == msg.sender,
+            "Only the contract deployer can add new owners."
+        );
         owners[_owner] = true;
     }
 
     function removeOwner(address _owner) public {
-        require(msg.sender == msg.sender, "Only the contract deployer can remove owners.");
+        require(
+            msg.sender == msg.sender,
+            "Only the contract deployer can remove owners."
+        );
         owners[_owner] = false;
     }
 
     function store(bytes memory _data) public {
         require(owners[msg.sender], "Sender is not an owner.");
         // Store the data
+        data[msg.sender] = _data;
     }
 
     function retrieve() public view returns (bytes memory) {
         require(owners[msg.sender], "Sender is not an owner.");
         // Retrieve and return the data
+        return data[msg.sender];
     }
 }
