4df4de12f6f51be434c3b59e291da39c0130240e
HughesSeanKyle <khughessean@yahoo.com>
2023-01-20 14:04:24 +0200

    first commit

---
+++ contracts/DeStore.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DecentralizedStorage {
+    mapping(address => bool) private owners;
+
+    function addOwner(address _owner) public {
+        require(msg.sender == msg.sender, "Only the contract deployer can add new owners.");
+        owners[_owner] = true;
+    }
+
+    function removeOwner(address _owner) public {
+        require(msg.sender == msg.sender, "Only the contract deployer can remove owners.");
+        owners[_owner] = false;
+    }
+
+    function store(bytes memory _data) public {
+        require(owners[msg.sender], "Sender is not an owner.");
+        // Store the data
+    }
+
+    function retrieve() public view returns (bytes memory) {
+        require(owners[msg.sender], "Sender is not an owner.");
+        // Retrieve and return the data
+    }
+}
